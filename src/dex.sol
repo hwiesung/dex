c8f6b8e134fb90915f265bd4b3f48633aacf14d4
hwiesung <hwiesung@gmail.com>
2018-11-14 16:33:33 +0900

    buy token

--- src/dex.sol
+++ src/dex.sol
@@ -72,29 +72,26 @@ contract EtherDex  {
     uint public feeMake; //percentage times (1 ether)
     uint public feeTake; //percentage times (1 ether)
     uint public feeRebate; //percentage times (1 ether)
-    mapping (address => mapping (address => uint)) public tokens; //mapping of token addresses to mapping of account balances (token=0 means Ether)
-    mapping (address => mapping (bytes32 => uint)) public orders; //mapping of user accounts to mapping of order hashes to booleans (true = submitted by user, equivalent to offchain signature)
-    mapping (address => mapping (bytes32 => uint)) public orderFills; //mapping of user accounts to mapping of order hashes to uints (amount of order that has been filled)
-    mapping (address => mapping (bytes32 => uint)) public orderPrices;
 
-    event SellOrder(bytes32 hash, address tokenGive, uint amountGive, uint price, uint expire, uint nonce, address user);
-    event Order(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user);
-    event Cancel(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s);
-    event Trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address get, address give);
-    event Deposit(address token, address user, uint amount, uint balance);
-    event Withdraw(address token, address user, uint amount, uint balance);
+    mapping (bytes32 => bool) public asks;
 
-    constructor(address admin_, address feeAccount_, uint feeMake_, uint feeTake_, uint feeRebate_) public {
+    event Ask(bytes32 hash, address tokenGive, uint amountGive, uint price, uint expire, uint nonce, address seller);
+    event Sold(bytes32 hash, address tokenGive, uint amountGive, uint price, address seller, address buyer);
+    event Cancel(bytes32 hash, address tokenGive, uint amountGive, uint price, address seller, address buyer);
+
+    event Deposit(address token, address user, uint amount);
+    event Withdraw(address token, address user, uint amount);
+
+    constructor(address admin_) public {
         admin = admin_;
-        feeAccount = feeAccount_;
-        feeMake = feeMake_;
-        feeTake = feeTake_;
-        feeRebate = feeRebate_;
+        feeAccount = address(0);
+        feeMake = 0;
+        feeTake = 0;
+        feeRebate = 0;
     }
 
     function () payable external {
-        tokens[address(0)][msg.sender] = tokens[address(0)][msg.sender].add( msg.value);
-        emit Deposit(address(0), msg.sender, msg.value, tokens[address(0)][msg.sender]);
+
     }
 
     function changeAdmin(address admin_) public{
@@ -125,100 +122,53 @@ contract EtherDex  {
         feeRebate = feeRebate_;
     }
 
-    function deposit()  payable external {
-        tokens[address(0)][msg.sender] = tokens[address(0)][msg.sender].add( msg.value);
-        emit Deposit(address(0), msg.sender, msg.value, tokens[address(0)][msg.sender]);
-    }
-
-    function withdraw(uint256 amount) public {
-        require (tokens[address(0)][msg.sender] >= amount);
-        tokens[address(0)][msg.sender] = tokens[address(0)][msg.sender].sub(amount);
-        require (msg.sender.call.value(amount)());
-        emit Withdraw(address(0), msg.sender, amount, tokens[address(0)][msg.sender]);
-    }
-
-    function depositToken(address token, uint256 amount) public{
+    function depositToken(address user, address token, uint256 amount) private returns(bool){
         require (token!=address(0));
-        require (Token(token).approve(this, amount) );
-        require (Token(token).transferFrom(msg.sender, this, amount) );
-        tokens[token][msg.sender] = tokens[token][msg.sender].add(amount);
-        emit Deposit(token, msg.sender, amount, tokens[token][msg.sender]);
-    }
+        require (Token(token).transferFrom(user, this, amount) );
 
-    function withdrawToken(address token, uint256 amount) public{
-        require (token!=address(0x0));
-        require (tokens[token][msg.sender] >= amount);
-        tokens[token][msg.sender] = tokens[token][msg.sender].sub(amount);
-        require (Token(token).transfer(msg.sender, amount));
-        emit Withdraw(token, msg.sender, amount, tokens[token][msg.sender]);
-    }
+        emit Deposit(token, user, amount);
 
-    function balanceOf(address token, address user) public view returns (uint256) {
-        return tokens[token][user];
-    }
-
-    function sellOrder(address tokenGive, uint amountGive, uint price, uint expire, uint nonce) public {
-        bytes32 hash = sha256(abi.encodePacked(this, tokenGive, amountGive, price, expire, nonce));
-        orders[msg.sender][hash] = amountGive;
-        orderPrices[msg.sender][hash] = price;
-        emit SellOrder(hash, tokenGive, amountGive, price, expire, nonce, msg.sender);
-    }
-
-
-    function trade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount) public {
-        //amount is in amountGet terms
-        bytes32 hash = sha256(abi.encodePacked(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce));
-        assert ((
-            (orders[user][hash]>0 && ecrecover(keccak256( abi.encodePacked("\x19Ethereum Signed Message:\n32", hash) ),v,r,s) == user) ||
-            block.number > expires ||
-            orderFills[user][hash].add(amount) > amountGet
-            ));
-        tradeBalances(tokenGet, amountGet, tokenGive, amountGive, user, amount);
-        orderFills[user][hash] = orderFills[user][hash].add(amount);
-        emit Trade(tokenGet, amount, tokenGive, amountGive * amount / amountGet, user, msg.sender);
-    }
-
-    function tradeBalances(address tokenGet, uint amountGet, address tokenGive, uint amountGive, address user, uint amount) private {
-        uint feeMakeXfer = amount.mul(feeMake) / (1 ether);
-        uint feeTakeXfer = amount.mul( feeTake) / (1 ether);
-        uint feeRebateXfer = 0;
-
-        tokens[tokenGet][msg.sender] = tokens[tokenGet][msg.sender].sub( amount.add(feeTakeXfer));
-        tokens[tokenGet][user] = tokens[tokenGet][user].add( amount.add(feeRebateXfer).sub( feeMakeXfer));
-        tokens[tokenGet][feeAccount] = tokens[tokenGet][feeAccount].add(feeMakeXfer.add(feeTakeXfer).sub(feeRebateXfer));
-        tokens[tokenGive][user] = tokens[tokenGive][user].sub(amountGive.mul( amount) / amountGet);
-        tokens[tokenGive][msg.sender] = tokens[tokenGive][msg.sender].add( amountGive.mul( amount) / amountGet);
-    }
-
-    function testTrade(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s, uint amount, address sender) public constant returns(bool) {
-        if (!(
-        tokens[tokenGet][sender] >= amount &&
-        availableVolume(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, user, v, r, s) >= amount
-        )) return false;
         return true;
     }
 
-    function availableVolume(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user, uint8 v, bytes32 r, bytes32 s) internal constant returns(uint) {
-        bytes32 hash = sha256(abi.encodePacked(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce));
-        if (!(
-        ( ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)),v,r,s) == user) &&
-        block.number <= expires
-        )) return 0;
-        uint available1 = amountGet.sub( orderFills[user][hash]);
-        uint available2 = amountGet.mul(tokens[tokenGive][user]) / amountGive;
-        if (available1<available2) return available1;
-        return available2;
+    function withdrawToken(address user, address token, uint256 amount) private returns(bool){
+        require (token!=address(0x0));
+        require (Token(token).transfer(user, amount));
+
+        emit Withdraw(token, user, amount);
+        return true;
     }
 
-    function amountFilled(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, address user) public constant returns(uint) {
-        bytes32 hash = sha256(abi.encodePacked(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce));
-        return orderFills[user][hash];
+    function askToken(address token, uint amount, uint price, uint expire, uint nonce) external {
+        bytes32 hash = sha256(abi.encodePacked(this, token, amount, price, expire, nonce));
+        uint256 total = price.mul( amount ).div(1 ether);
+        require( total >= (0.05 ether) && total < (10 ether) );
+        require(depositToken(msg.sender, token, amount));
+
+        asks[hash] = true;
+
+        emit Ask(hash, token, amount, price, expire, nonce, msg.sender);
     }
 
-    function cancelOrder(address tokenGet, uint amountGet, address tokenGive, uint amountGive, uint expires, uint nonce, uint8 v, bytes32 r, bytes32 s) public {
-        bytes32 hash = sha256(abi.encodePacked(this, tokenGet, amountGet, tokenGive, amountGive, expires, nonce));
-        assert ((orders[msg.sender][hash] > 0 && ecrecover(keccak256(abi.encodePacked("\x19Ethereum Signed Message:\n32", hash)),v,r,s) != msg.sender));
-        orderFills[msg.sender][hash] = amountGet;
-        emit Cancel(tokenGet, amountGet, tokenGive, amountGive, expires, nonce, msg.sender, v, r, s);
+    function buy(bytes32 targetHash, address targetToken, uint targetAmount, uint targetPrice, uint targetExpire,  uint targetNone, address seller) payable external {
+        bytes32 hash = sha256(abi.encodePacked(this, targetToken, targetAmount, targetPrice, targetExpire, targetNone));
+        uint256 total = targetPrice.mul( targetAmount ).div(1 ether);
+        require( asks[hash] && targetHash == hash && block.number <= targetExpire );
+        require( total == msg.value );
+        require( seller.call.value(msg.value)() );
+        require( withdrawToken(msg.sender, targetToken, targetAmount) );
+
+        asks[hash] = false;
+
+        bytes32 soldHash = sha256(abi.encodePacked(hash, msg.sender));
+
+        emit Sold(soldHash, targetToken, targetAmount, targetPrice, seller, msg.sender);
+
     }
+
+    function cancelOrder(bytes32 targetHash, address targetToken, uint targetAmount, uint targetPrice, uint targetExpire,  uint targetNone) external {
+        emit Cancel(soldHash, targetToken, targetAmount, targetPrice, seller, msg.sender);
+    }
+
+
 }
