86df39796f32976b80c73656601a3f67c6a7bb00
hwiesung <hwiesung@gmail.com>
2018-11-15 10:10:56 +0900

    charge fee

--- src/dex.sol
+++ src/dex.sol
@@ -69,20 +69,22 @@ contract TokenMarket  {
 
     address public admin;
     address public feeAccount;
-    uint public makerFee; //for seller
-    uint public takerFee; //for buyer
+    uint256 public makerFee; //for seller
+    uint256 public takerFee; //for buyer
+
+    mapping (address => uint) public incomes;
 
     mapping (bytes32 => bool) public asks;
 
-    event Ask(bytes32 hash, address token, uint amount, uint price, uint total, uint expire, uint nonce, address indexed seller);
-    event Sold(bytes32 hash, address token, uint amount, uint price, address indexed seller, address indexed buyer);
-    event Cancel(bytes32 hash, address token, uint amount, uint price, address indexed seller);
+    event Ask(bytes32 hash, address token, uint256 amount, uint256 price, uint256 total, uint256 expire, uint256 nonce, address indexed seller);
+    event Sold(bytes32 hash, address token, uint256 amount, uint256 price, address indexed seller, address indexed buyer);
+    event Cancel(bytes32 hash, address token, uint256 amount, uint256 price, address indexed seller);
 
 
-    constructor() public {
+    constructor(address _feeAccount) public {
         admin = msg.sender;
-        feeAccount = msg.sender;
-        makerFee = 0;
+        feeAccount = _feeAccount;
+        makerFee = 0;   // 1 means 0.1% fee
         takerFee = 0;
     }
 
@@ -101,16 +103,32 @@ contract TokenMarket  {
     }
 
 
-    function changeMakerFee(uint _makerFee) public{
+    function changeMakerFee(uint256 _makerFee) public{
         require (msg.sender == admin) ;
         require (_makerFee != makerFee) ;
         makerFee = _makerFee;
     }
 
-    function changeTakerFee(uint _takerFee) public{
+    function changeTakerFee(uint256 _takerFee) public{
         require (msg.sender == admin) ;
         require (_takerFee != takerFee) ;
         takerFee = _takerFee;
+    }
+
+    function withdrawFee(uint256 amount) public returns(bool){
+        require (msg.sender == admin) ;
+        require ( feeAccount != address(0) ) ;
+        feeAccount.call.value(amount)();
+
+        return true;
+    }
+
+    function withdrawTokenFee(address token, uint256 amount) public returns(bool){
+        require (msg.sender == admin) ;
+        require ( feeAccount != address(0) ) ;
+        withdrawToken(feeAccount, token, amount);
+
+        return true;
     }
 
     function depositToken(address user, address token, uint256 amount) private returns(bool){
@@ -127,7 +145,7 @@ contract TokenMarket  {
         return true;
     }
 
-    function askToken(address token, uint amount, uint price, uint expire, uint nonce) external {
+    function askToken(address token, uint256 amount, uint256 price, uint256 expire, uint256 nonce) external {
         bytes32 hash = sha256(abi.encodePacked(this, token, amount, price, expire, nonce, msg.sender));
         uint256 total = price.mul( amount ).div(1 ether);
         require( !asks[hash] );
@@ -139,12 +157,28 @@ contract TokenMarket  {
         emit Ask(hash, token, amount, price, total, expire, nonce, msg.sender);
     }
 
-    function buy(bytes32 targetHash, address targetToken, uint targetAmount, uint targetPrice, uint targetTotal, uint targetExpire,  uint targetNonce, address seller) payable external {
+    function buy(bytes32 targetHash, address targetToken, uint256 targetAmount, uint256 targetPrice, uint256 targetTotal, uint256 targetExpire,  uint256 targetNonce, address seller) payable external {
         bytes32 hash = sha256(abi.encodePacked(this, targetToken, targetAmount, targetPrice, targetExpire, targetNonce, seller));
         require( asks[hash] && targetHash == hash && block.number <= targetExpire );
         require( targetTotal == msg.value );
-        require( seller.call.value(msg.value)() );
-        require( withdrawToken(msg.sender, targetToken, targetAmount) );
+
+        uint256 makerGet = msg.value;
+        uint256 takerGet = targetAmount;
+        uint256 fee = 0;
+        if(makerFee != 0 ){
+            fee = makerGet.mul(makerFee).div(1000);
+            makerGet = makerGet.sub(fee);
+            incomes[address(0)].add(fee);
+        }
+
+        if(takerFee != 0 ){
+            fee = takerGet.mul(takerFee).div(1000);
+            takerGet = takerGet.sub(fee);
+            incomes[targetToken].add(fee);
+        }
+
+        require( seller.call.value(makerGet)() );
+        require( withdrawToken(msg.sender, targetToken, takerGet) );
 
         asks[hash] = false;
 
@@ -154,7 +188,7 @@ contract TokenMarket  {
 
     }
 
-    function cancelOrder(bytes32 targetHash, address targetToken, uint targetAmount, uint targetPrice, uint targetExpire, uint targetNonce) external {
+    function cancelOrder(bytes32 targetHash, address targetToken, uint256 targetAmount, uint256 targetPrice, uint256 targetExpire, uint256 targetNonce) external {
         bytes32 hash = sha256(abi.encodePacked(this, targetToken, targetAmount, targetPrice, targetExpire, targetNonce, msg.sender));
         require( asks[hash] && targetHash == hash );
         require( withdrawToken(msg.sender, targetToken, targetAmount) );
